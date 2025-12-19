a767fa70e02baa44dbf1f00c49b26f76d5f6e103
Ilja <lyfarilya@gmail.com>
2025-12-19 22:36:16 +0300

    refactor(mvc): simplify MainController implementation

--- src/controller/mainController.cpp
+++ src/controller/mainController.cpp
@@ -1,95 +1,25 @@
 #include "mainController.h"
-#include "clientController.h"
-#include "serverController.h"
-#include <QDebug>
 
-MainController::MainController(QObject* parent) : QObject(parent)
+#include "menuWidget.h"
+#include "mainGameScreen.h"
+
+MainController::MainController(GameModel* model, QObject* parent)
+    : QObject(parent),
+    m_model(model),
+    m_network(nullptr),
+    m_menu(nullptr),
+    m_gameScreen(nullptr)
 {
-    session = new GameSession(this);
-    menu = new MenuWidget();
-    mainGameScreen = new MainGameScreen();
-
-
-
-
-    //хост
-    connect(menu, &MenuWidget::startGameClicked, this, [this](){
-        session->getLocalPlayer()->setIsServer(true);
-        session->getLocalPlayer()->setId(1);
-        session->getLocalPlayer()->setName(menu->getName().isEmpty() ? "Player" : menu->getName());
-        session->startGameAsHost(true);
-
-        auto net = session->getNetwork();
-        if(net){
-            initChatController(true);
-            connect(chatController, &ChatController::sendMessageToNetwork,
-                    static_cast<ServerController*>(net), &ServerController::broadcast,
-                    Qt::UniqueConnection);
-
-            connect(static_cast<ServerController*>(net), &ServerController::sendMessageToChatController,
-                    chatController, &ChatController::onNetworkMessage,
-                    Qt::UniqueConnection);
-
-            connect(static_cast<ServerController*>(net), &ServerController::updatePlayers, this, &MainController::updatePlayersOnScreen);
-            emit gameScreenRequested();
-
-            static_cast<ServerController*>(net)->emitInitialPlayers();
-        }
-    });
-
-    //клиент
-    connect(menu, &MenuWidget::connectClicked, this, [this](){
-        session->getLocalPlayer()->setName(menu->getName().isEmpty() ? "Player" : menu->getName());
-        session->getLocalPlayer()->setIsServer(false);
-        QString ip = menu->getIp();
-        session->connectToGame(ip);
-        auto net = session->getNetwork();
-
-        if(net){
-            connect(static_cast<ClientController*>(net), &ClientController::connected, this, [this, net](){
-                initChatController(false);
-                connect(chatController, &ChatController::sendMessageToNetwork,
-                        static_cast<ClientController*>(net), &ClientController::sendChatMessage,
-                        Qt::UniqueConnection);
-
-                connect(static_cast<ClientController*>(net), &ClientController::sendMessageToChatController,
-                        chatController, &ChatController::onNetworkMessage,
-                        Qt::UniqueConnection);
-
-                connect(static_cast<ClientController*>(net), &ClientController::updatePlayers,
-                        this, &MainController::updatePlayersOnScreen,
-                        Qt::UniqueConnection);
-            });
-            emit gameScreenRequested();
-        }
-    });
-
-
+    m_menu = new MenuWidget();
+    m_gameScreen = new MainGameScreen();
 }
 
-
-
-void MainController::initChatController(bool isServer)
+MenuWidget* MainController::menu() const
 {
-    chatController = new ChatController(session->getLocalPlayer()->getId(),
-                                        session->getLocalPlayer()->getName(),
-                                        isServer, this);
-    mainGameScreen->setChatController(chatController);
-
-    connect(mainGameScreen->getChatWidget(), &ChatWidget::sendMessage,
-            chatController, &ChatController::sendChatMessage,
-            Qt::UniqueConnection);
-
-    connect(chatController, &ChatController::newMessageFromNetwork,
-            mainGameScreen->getChatWidget(), &ChatWidget::displayMessage,
-            Qt::UniqueConnection);
+    return m_menu;
 }
 
-void MainController::updatePlayersOnScreen(const QVector<PlayerModel>& players)
+MainGameScreen* MainController::gameScreen() const
 {
-    qDebug() << "[MainController] Updating players on screen";
-    mainGameScreen->getLeftBarWidget()->updatePlayers(players);
+    return m_gameScreen;
 }
-
-
-
